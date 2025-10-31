b1bdb38a0f927a4851ab8b3c27f1e0fa04e08f7f
Sayantan Roy <66847886+roy-369@users.noreply.github.com>
2025-10-31 15:29:56 +0100

    Add files via upload

---
+++ QMC data analysis codes/Data analytic continuation green functions/Gaussian default model/Green_function_real_space_analytic_continuation_gaussian.sh
@@ -0,0 +1,129 @@
+#!/bin/bash
+
+N=16
+dt=0.05
+Freq=2501
+omega_max=25.0
+omega_min=-25.0
+
+
+cd /fs/byo/trivedi/roy.369/Research/Determinant_Quantum_Monte_Carlo/UC_code/legacy-qmc/Cold_atom_correlators/Text_files/Text_files_N_${N}_analytic_continuation
+
+for U in 3.00 3.50 4.00 4.50 5.00 5.50 6.00 #6.50 7.00 7.50 8.00 8.50 9.00 9.50 10.00  
+do
+   cd Text_files_N_${N}_U_${U}_dtau_${dt} || exit 
+
+   for Mu in 0.00 #$(seq 0.00 0.20 5.00)
+   do
+        cd Mu_${Mu} || exit
+
+	for trot_slices in 20 22 24 26 28 30 32 34 36 38 40 42 44 46 48 50
+        do
+               Beta_val=$(echo $dt*$trot_slices | bc)
+               Timestep=$(echo $trot_slices+1 | bc)
+
+               cd dtau_${dt}_L_${trot_slices} || exit
+
+               for Sig in 2.0
+               do
+
+                  mkdir -p Spectral_functions_gaussian_sigma_${Sig}_nfreq_${Freq}
+                  cd Spectral_functions_gaussian_sigma_${Sig}_nfreq_${Freq} || exit
+                 
+                     
+                  for rlab in 0
+                  do
+                      
+                      mkdir -p r_point_$rlab
+                      cd r_point_$rlab
+
+                      cp /fs/byo/trivedi/roy.369/Research/Determinant_Quantum_Monte_Carlo/UC_code/legacy-qmc/Cold_atom_correlators/Text_files/Text_files_N_${N}_analytic_continuation/Text_files_N_${N}_U_${U}_dtau_${dt}/Mu_${Mu}/dtau_${dt}_L_${trot_slices}/Retarded_green_functions_real_space_averaged/Retarded_Green_function_real_space_avg_N_${N}_U_${U}_mu_${Mu}_dtau_${dt}_L_${trot_slices}_r_label_${rlab}.dat ./Retarded_green_function_real_space_avg_N_${N}_U_${U}_mu_${Mu}_dtau_${dt}_L_${trot_slices}_r_label_${rlab}_ac.dat
+
+                      cp /fs/byo/trivedi/roy.369/Research/Determinant_Quantum_Monte_Carlo/UC_code/legacy-qmc/Cold_atom_correlators/Data_processing_codes/Data_analytic_continuation_green_functions/Gaussian_default_model/time_gaussian_temp.param ./time_gaussian.param
+ 
+                      data_file=Retarded_green_function_real_space_avg_N_${N}_U_${U}_mu_${Mu}_dtau_${dt}_L_${trot_slices}_r_label_${rlab}_ac.dat
+                      out_file=Spectral_function_real_space_avg_N_${N}_U_${U}_mu_${Mu}_dtau_${dt}_L_${trot_slices}_r_label_${rlab}_gaussian_sigma_${Sig}_nfreq_${Freq}_omega_max_${omega_max}_omega_min_${omega_min}.dat
+                      out_file_back_cont=Retarded_green_function_back_continued_real_space_avg_N_${N}_U_${U}_mu_${Mu}_dtau_${dt}_L_${trot_slices}_r_label_${rlab}_gaussian_sigma_${Sig}_nfreq_${Freq}_omega_max_${omega_max}_omega_min_${omega_min}.dat
+                      out_file_data=Analytic_continuation_data_N_${N}_U_${U}_mu_${Mu}_dtau_${dt}_L_${trot_slices}_r_label_${rlab}_gaussian_sigma_${Sig}_nfreq_${Freq}_omega_max_${omega_max}_omega_min_${omega_min}.h5
+                      out_file_prob=Posterior_probabilities_N_${N}_U_${U}_mu_${Mu}_dtau_${dt}_L_${trot_slices}_r_label_${rlab}_gaussian_sigma_${Sig}_nfreq_${Freq}_omega_max_${omega_max}_omega_min_${omega_min}.dat
+                      out_file_class_max=Spectral_function_real_space_avg_classical_mxnt_N_${N}_U_${U}_mu_${Mu}_dtau_${dt}_L_${trot_slices}_r_label_${rlab}_gaussian_sigma_${Sig}_nfreq_${Freq}_omega_max_${omega_max}_omega_min_${omega_min}.dat
+                      out_file_hist_max=Spectral_function_real_space_avg_historic_mxnt_N_${N}_U_${U}_mu_${Mu}_dtau_${dt}_L_${trot_slices}_r_label_${rlab}_gaussian_sigma_${Sig}_nfreq_${Freq}_omega_max_${omega_max}_omega_min_${omega_min}.dat
+ 
+mydir=$(pwd)
+rawjob="$(cat <<EOF
+#!/bin/sh
+#SBATCH --job-name=l$trot_slices-Mu$Mu
+#SBATCH -N 1
+#SBATCH --ntasks=1
+#SBATCH --partition trivedi,trivedi-sapphire,batch
+#SBATCH -t 32:00:00                    
+#SBATCH --mem=4G
+#SBATCH --mail-type=FAIL                        
+#SBATCH --mail-user=roy.369@osu.edu                             
+hostname
+#SBATCH --no-requeue
+module load CQMP-maxent/2.0
+cd \$SLURM_SUBMIT_DIR
+time
+                     
+                            
+sed -i 's/beta/'$Beta_val'/g' ./time_gaussian.param
+sed -i 's/Tstep/'$Timestep'/g' ./time_gaussian.param
+sed -i 's/Omega/'$Freq'/g' ./time_gaussian.param
+sed -i 's/sigma/'$Sig'/g' ./time_gaussian.param
+sed -i 's/fname/'$data_file'/g' ./time_gaussian.param
+sed -i 's/om_max/'$omega_max'/g' ./time_gaussian.param
+sed -i 's/om_min/'$omega_min'/g' ./time_gaussian.param
+
+maxent time_gaussian.param &>k_label_${klab}_output_gaussian.out
+                         
+sleep 5
+cp time_gaussian.out.avspec.dat $out_file
+rm time_gaussian.out.avspec.dat
+cp time_gaussian.out.avspec_back.dat $out_file_back_cont
+rm time_gaussian.out.avspec_back.dat
+rm time_gaussian.out.chi2.dat
+cp time_gaussian.out.chispec.dat $out_file_hist_max
+rm time_gaussian.out.chispec.dat
+rm time_gaussian.out.chispec_back.dat
+rm time_gaussian.out.fits.dat
+cp time_gaussian.out.maxspec.dat $out_file_class_max
+rm time_gaussian.out.maxspec.dat
+rm time_gaussian.out.maxspec_back.dat
+cp time_gaussian.out.out.h5 $out_file_data
+rm time_gaussian.out.out.h5
+cp time_gaussian.out.prob.dat $out_file_prob
+rm time_gaussian.out.prob.dat
+rm time_gaussian.out.spex.dat
+      
+time
+EOF
+)"
+
+                         echo "$rawjob" &> job.bat
+                         sbatch job.bat
+                      cd ..
+                      done
+#loop for k ends here                         
+        
+                  echo "Sigma_$Sig"
+                  cd ..     
+                  done
+#loop for sigma ends here
+                 
+               cd ..
+               done
+#loop for beta ends here
+               
+             cd ..
+             done
+#loop for Mu ends here
+             
+        cd ..               
+        echo "U_$U, Mu_$Mu, L_$trot_slices"
+      
+        done
+#loop for U ends here
+
+                 
+
